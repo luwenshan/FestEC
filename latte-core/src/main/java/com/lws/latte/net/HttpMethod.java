42aa95eb20f916bee684cec41166034ce8bf51cc
lws <lws1076694749@126.com>
2018-01-04 23:51:23 +0800

    网络请求框架1

---
+++ latte-core/src/main/java/com/lws/latte/net/HttpMethod.java
@@ -0,0 +1,15 @@
+package com.lws.latte.net;
+
+/**
+ * Created by Wenshan.Lu on 2018/1/4.
+ */
+
+public enum HttpMethod {
+    GET,
+    POST,
+    POST_RAW,
+    PUT,
+    PUT_RAW,
+    DELETE,
+    UPLOAD
+}
---
+++ latte-core/src/main/java/com/lws/latte/net/RestClient.java
@@ -0,0 +1,8 @@
+package com.lws.latte.net;
+
+/**
+ * Created by Wenshan.Lu on 2018/1/4.
+ */
+
+public class RestClient {
+}
